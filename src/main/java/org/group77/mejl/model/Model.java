9d183b604dc5ca8f9328f4c4512b65a824ac44e3
Fredins <fredin.martin@gmail.com>
2021-09-07 19:42:35 +0200

    moved and refactored to/in SystemManager

--- src/main/java/org/group77/mejl/model/Model.java
+++ src/main/java/org/group77/mejl/model/Model.java
@@ -1,47 +1,11 @@
 package org.group77.mejl.model;
 
 import javax.mail.*;
-import java.io.*;
-import java.util.Properties;
-import java.util.Scanner;
 
 // THIS CLASS WILL BE SPLIT INTO MULTIPLE SINGLE RESPONSIBILITY CLASSES ONCE
 // SOME THINGS ARE FIGURED OUT
 public class Model {
 
-    private String getESPsDir(){
-       return getDataDir() + "esp.d\\";
-    }
-    private String getDataDir(){
-        return "C:\\Users\\Martin\\AppData\\Local\\grupp77\\mejl\\";
-    }
-
-
-    private void createFile(String path) throws IOException {
-        File file = new File(path);
-        if (file.exists()) {
-            file.delete();
-        }
-        file.createNewFile();
-    }
-
-    private <T> void writeTo(T o, String path) throws IOException {
-        FileOutputStream file = new FileOutputStream(path);
-        ObjectOutputStream out = new ObjectOutputStream(file);
-        out.writeObject(o);
-        out.close();
-        file.close();
-    }
-
-
-    private <T> T readFrom(String path) throws IOException, ClassNotFoundException {
-        FileInputStream file = new FileInputStream(path);
-        ObjectInputStream in = new ObjectInputStream(file);
-        T o = (T) in.readObject();
-        in.close();
-        file.close();
-        return o;
-    }
 
 
     private Folder[] getFolders(Store store) throws MessagingException {
--- src/main/java/org/group77/mejl/model/SystemManager.java
+++ src/main/java/org/group77/mejl/model/SystemManager.java
@@ -1,4 +1,39 @@
 package org.group77.mejl.model;
 
+import java.io.*;
+
 public class SystemManager {
+    protected String getAccountDir(){
+        return getDataDir() + "esp.d\\";
+    }
+    protected String getDataDir(){
+        return "C:\\Users\\Martin\\AppData\\Local\\grupp77\\mejl\\";
+    }
+
+    protected void createFile(String path) throws IOException {
+        File file = new File(path);
+        if (file.exists()) {
+            file.delete();
+        }
+        file.createNewFile();
+    }
+
+    protected <T> void writeTo(T o, String path) throws IOException {
+        FileOutputStream file = new FileOutputStream(path);
+        ObjectOutputStream out = new ObjectOutputStream(file);
+        out.writeObject(o);
+        out.close();
+        file.close();
+    }
+
+
+    protected <T> T readFrom(String path) throws IOException, ClassNotFoundException {
+        FileInputStream file = new FileInputStream(path);
+        ObjectInputStream in = new ObjectInputStream(file);
+        T o = (T) in.readObject();
+        in.close();
+        file.close();
+        return o;
+    }
+
 }
