3fa6952f50a908b6e50676c4dff17eacf776671a
DavidZamanian <54624136+DavidZamanian@users.noreply.github.com>
2021-09-07 16:56:11 +0200

    Added testConnection method

--- src/main/java/org/group77/mejl/controllers/AccountController.java
+++ src/main/java/org/group77/mejl/controllers/AccountController.java
@@ -3,6 +3,7 @@ package org.group77.mejl.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import org.group77.mejl.model.AccountInformation;
+import org.group77.mejl.model.EmailApp;
 import org.group77.mejl.model.Model;
 
 public class AccountController {
@@ -20,9 +21,23 @@ public class AccountController {
     private TextField identifier;
 
     private final Model model = new Model();
+    private EmailApp emailApp = new EmailApp();
+
 
     @FXML
     private void addEmail(){
+        boolean connection = emailApp.testConnection(identifier.getText(), host.getText(), Integer.parseInt(port.getText()), protocol.getText(), user.getText(), password.getText());
+        if (connection) {
+
+        } else{
+            System.out.println("can't connect to email service provider");
+        }
+    }
+
+
+
+    @FXML
+    private void addEmaill(){
         AccountInformation accountInformation = new AccountInformation(
                 identifier.getText(),
                 host.getText(),
--- src/main/java/org/group77/mejl/model/EmailApp.java
+++ src/main/java/org/group77/mejl/model/EmailApp.java
@@ -1,4 +1,18 @@
 package org.group77.mejl.model;
 
 public class EmailApp {
+
+    Connector connector = new Connector();
+    EmailBuilder emailBuilder = new EmailBuilder();
+    Storage storage = new Storage();
+    AccountHandler accountHandler = new AccountHandler();
+
+
+    public boolean testConnection(String identifier, String host, Integer port, String protocol, String user, String password ){
+        return connector.testConnection(identifier, host, port, protocol, user, password);
+    }
+
+
+
+
 }
