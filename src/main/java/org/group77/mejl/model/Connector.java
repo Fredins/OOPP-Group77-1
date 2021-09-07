22aebd2386d0947c5da238d725a4eeb532be7076
DavidZamanian <54624136+DavidZamanian@users.noreply.github.com>
2021-09-07 21:51:31 +0200

    Update Connector.java

--- src/main/java/org/group77/mejl/model/Connector.java
+++ src/main/java/org/group77/mejl/model/Connector.java
@@ -2,14 +2,4 @@ package org.group77.mejl.model;
 
 public class Connector {
 
-    /**
-     * Added skeleton just to be able to make method in EmailApp
-     *
-     * @return
-     */
-
-    public boolean testConnection(String identifier, String host, Integer port, String protocol, String user, String password ){
-        return false;
-    }
-
 }
