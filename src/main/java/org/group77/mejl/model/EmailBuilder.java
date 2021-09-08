d44de58b8510f87078c6292ce265141239f10027
04320 <alexey.ryabov@hotmail.com>
2021-09-08 18:32:30 +0200

    Update EmailBuilder.java

--- src/main/java/org/group77/mejl/model/EmailBuilder.java
+++ src/main/java/org/group77/mejl/model/EmailBuilder.java
@@ -1,4 +1,22 @@
 package org.group77.mejl.model;
 
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * EmailBuilder is responsible for composing a email message.
+ *
+ * To be able to send an email following need to happen:
+ *  1) Get a Session.
+ *  2) Create a default MimeMessage object.
+ *  3) Set From, To, Subject in the message.
+ *  4) Set the message.
+ *
+ * **/
 public class EmailBuilder {
+
+
 }
