3344f68ac8b3825379ba02b8de37225ed52bbd34
04320 <alexey.ryabov@hotmail.com>
2021-09-23 12:40:23 +0200

    Update AccountHandler.java
    Was working on AccountHandler: storeAccount() method.

--- src/main/java/org/group77/mejl/model/AccountHandler.java
+++ src/main/java/org/group77/mejl/model/AccountHandler.java
@@ -1,13 +1,20 @@
 package org.group77.mejl.model;
 import java.util.*;
 public class AccountHandler {
-
+/*
+* @author
+* */
     Account activeAccount;
     Storage storage;
     AccountFactory accountFactory;
 
-    public boolean storeAccount(Account account) {
-        return false;
+    /* @author Alexey Ryabov
+    * TODO Test this method */
+    public boolean storeAccount(Account account) throws Exception {
+        try {
+            storage.store(account);
+            return true;
+        } catch (Exception e) {throw new Exception("Failed in AccountHandler -> storeAccount -method ! ");}
     }
 
     public Account getAccount(String emailAddress) {
