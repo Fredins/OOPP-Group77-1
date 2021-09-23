aa8af670047eae9a10442d45111e54cdb644f0e0
Hampus Jernkrook <hampus.e.jernkrook@gmail.com>
2021-09-23 14:09:49 +0200

    added tests for the implementation of getEmailServiceProvider and set a new version for junit in pom file

---
+++ src/test/java/org/group77/mejl/TestEmailServiceProviderFactory.java
@@ -0,0 +1,24 @@
+package org.group77.mejl;
+
+import org.group77.mejl.model.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestEmailServiceProviderFactory {
+    /*
+    @Test
+    public void TestGmailAccount() {
+        AccountFactory accountFactory = new AccountFactory();
+        Account account = accountFactory.createAccount("lol@gmail.com", "1234");
+        EmailServiceProviderFactory espFactory = new EmailServiceProviderFactory();
+        EmailServiceProviderStrategy esp = espFactory.getEmailServiceProvider(account);
+        assertTrue(esp instanceof GmailProvider);
+    }
+     */
+}
