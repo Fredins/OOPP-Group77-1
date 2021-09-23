9192016ee1bd07ba7029c6adbbd5fdf26efc23f8
elinhagman <73999747+elinhagman@users.noreply.github.com>
2021-09-23 12:38:31 +0200

    created addAccount method

--- src/main/java/org/group77/mejl/controller/AddAccountController.java
+++ src/main/java/org/group77/mejl/controller/AddAccountController.java
@@ -1,13 +1,24 @@
 package org.group77.mejl.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import org.group77.mejl.model.ApplicationManager;
 
 public class AddAccountController {
 
-    ApplicationManager appManager;
+    ApplicationManager applicationManager;
 
     @FXML
-    public void addAccount(){};
+    private TextField password;
+
+    @FXML
+    private TextField user;
+
+
+    @FXML
+    void addAccount(MouseEvent event) {
+        applicationManager.addAccount(user.getText(),password.getText());
+    }
 
 }
