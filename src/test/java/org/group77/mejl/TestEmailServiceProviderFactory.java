9d44b7918e21e496a087cac89999fcb001b6c7bf
Hampus Jernkrook <hampus.e.jernkrook@gmail.com>
2021-09-23 14:30:27 +0200

    added out-commented test for microsoft account.Comment back in once accountFactory is up to date

--- src/test/java/org/group77/mejl/TestEmailServiceProviderFactory.java
+++ src/test/java/org/group77/mejl/TestEmailServiceProviderFactory.java
@@ -10,8 +10,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author Hampus Jernkrook
+ */
 public class TestEmailServiceProviderFactory {
-    /*
+    // test that gmail account gets associated with a gmail provider
     @Test
     public void TestGmailAccount() {
         AccountFactory accountFactory = new AccountFactory();
@@ -20,5 +23,17 @@ public class TestEmailServiceProviderFactory {
         EmailServiceProviderStrategy esp = espFactory.getEmailServiceProvider(account);
         assertTrue(esp instanceof GmailProvider);
     }
+
+    /*
+    // test that microsoft (hotmail here) account gets associated with a microsoft provider
+    @Test
+    public void TestMicrosoftAccount() {
+        AccountFactory accountFactory = new AccountFactory();
+        Account account = accountFactory.createAccount("lol@hotmail.com", "1234");
+        EmailServiceProviderFactory espFactory = new EmailServiceProviderFactory();
+        EmailServiceProviderStrategy esp = espFactory.getEmailServiceProvider(account);
+        assertTrue(esp instanceof MicrosoftProvider);
+    }
      */
+
 }
