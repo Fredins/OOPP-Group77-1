519a8c7b5ab99a6de5a15acce2bf877edc85b5a1
Fredins <fredin.martin@gmail.com>
2021-09-26 18:05:35 +0200

    added dep javax.activation to use MimeUtility

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module org.group77.mejl {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.mail;
+    requires activation;
 
-    opens org.group77.mejl to javafx.fxml;
     exports org.group77.mejl;
     exports org.group77.mejl.controller;
     opens org.group77.mejl.controller to javafx.fxml;
