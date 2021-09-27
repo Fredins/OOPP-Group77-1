6bd8fd64e0cb36c5bba5410da0f00ca9e42b8c8e
Hampus Jernkrook <hampus.e.jernkrook@gmail.com>
2021-09-27 14:49:32 +0200

    wrote test for retrieveEmails

--- src/test/java/org/group77/mejl/TestLocalDiskStorage.java
+++ src/test/java/org/group77/mejl/TestLocalDiskStorage.java
@@ -1,14 +1,44 @@
 package org.group77.mejl;
 import org.group77.mejl.model.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestLocalDiskStorage {
 
-
     @Test
-    void testRetrieveEmails() throws Exception {
+    /**
+     * @author Hampus Jernkrook
+     */
+    void testRetrieveEmails() throws OSNotFoundException, IOException, ClassNotFoundException {
+        String testAddress = "TESTING-ADDRESS@gmail.com";
+        List<String> to = Arrays.asList("lol@gmail.com",
+                "lol2@gmail.com",
+                "lol3@gmail.com");
+        LocalDiscStorage storage = new LocalDiscStorage();
+        List<Email> emails = Arrays.asList(
+                new Email(testAddress, to, "1", "Email 1"),
+                new Email(testAddress, to, "2", "Email 2"),
+                new Email(testAddress, to, "3", "Email 3"),
+                new Email(testAddress, to, "4", "Email 4")
+                );
+        String folderName = "testFolder";
+        Folder testFolder = new Folder(folderName, emails);
+        List<Folder> folders = Arrays.asList(testFolder);
+        // store away
+        storage.store(testAddress, folders);
+        // now retrieve
+        List<Email> retrieved = storage.retrieveEmails(testAddress, folderName);
+        Email[] retrievedArray = (Email[]) retrieved.toArray(); //TODO will the casting work?
+        // check that the emails are the same //TODO WILL THIS WORK?
+        Assertions.assertArrayEquals(emails.toArray(), retrievedArray);
+    }
+
+    @Test //TODO keep or remove this one?
+    void testRetrieveEmails2() throws Exception {
         LocalDiscStorage storage = new LocalDiscStorage();
         AccountHandler handler = new AccountHandler();
         Account a = handler.createAccount("77grupp@gmail.com", "grupp77group");
@@ -20,7 +50,7 @@ public class TestLocalDiskStorage {
         // TODO assertion
     }
 
-    @Test
+    @Test //TODO who wrote this and what do you want to test??
     void testStorage() throws Exception
     {
         AccountHandler h = new AccountHandler();
