53ab3185c7857e074720dc0e946a99df13950b34
Hampus Jernkrook <hampus.e.jernkrook@gmail.com>
2021-09-28 16:27:48 +0200

    implemented search algorithm and one more test

--- src/main/java/org/group77/mejl/model/TextFinder.java
+++ src/main/java/org/group77/mejl/model/TextFinder.java
@@ -1,13 +1,46 @@
 package org.group77.mejl.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * This class implements an algorithm for searching emails for a given substring.
  * This substring can be in either one of the email addresses, the subject or the content.
+ *
+ * @author Hampus Jernkrook
  */
 public class TextFinder {
+    /**
+     * Searches a list of Emails for a given substring. The search is case-insensitive.
+     * @param emails - the list of emails to search.
+     * @param substring - the substring to search for.
+     * @return A subset of the input list `emails`, with Emails containing the given substring.
+     *
+     * @author Hampus Jernkrook
+     */
     public List<Email> search(List<Email> emails, String substring) {
-        return null;
+        // initialize list to store result
+        List<Email> res = new ArrayList<>();
+        // convert substring to lower case to ensure case-insensitiveness.
+        substring = substring.toLowerCase();
+        // for each email, if either from, one of the to:s, subject or content contains
+        // the substring, then add that email to res.
+        // Convert all strings to lowercase to ensure case-insensitiveness.
+        for (Email email : emails) {
+            // check everything except `to`
+            if (email.getFrom().toLowerCase().contains(substring) || email.getSubject().toLowerCase().contains(substring)
+                    || email.getContent().toLowerCase().contains(substring)) {
+                res.add(email);
+            } // if neither above contained substring, then check the list of to:s
+            else {
+                for (String to : email.getTo()) {
+                    if (to.toLowerCase().contains(substring)) {
+                        res.add(email);
+                        break; // break out of loop to avoid adding the same email more than once.
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
--- src/test/java/org/group77/mejl/TestTextFinder.java
+++ src/test/java/org/group77/mejl/TestTextFinder.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Tests for the TextFinder class.
+ * @author Hampus Jernkrook
+ */
 public class TestTextFinder {
     @Test
     public void testFindsBothUpperAndLowerCase() {
@@ -99,6 +103,32 @@ public class TestTextFinder {
         Email[] inputArray = input.toArray(new Email[0]);
         // expect the last element of input in output
         Email[] expected = new Email[]{inputArray[2]};
+        // check that output contains the last element
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testFindsUpperCasedSubstring() {
+        // set up
+        List<String> to = Arrays.asList("TEST_nr_1@gmail.com",
+                "TEST_nr_2@gmail.com",
+                "TEST_nr_3@gmail.com"
+        );
+        List<Email> input = Arrays.asList(
+                new Email("lol@gmail.com", List.of("TEST_nr_1@gmail.com"), "Upper", "contains SAUSAGE"),
+                new Email("lol@gmail.com", to, "Lower", "contains sausage"),
+                new Email("lol@gmail.com", List.of("TEST_nr_1@gmail.com"), "Nothing", "contains no meat at all")
+        );
+        TextFinder searcher = new TextFinder();
+        // search emails for part of second element's to, but upper-cased.
+        List<Email> res = searcher.search(input, "NR_2");
+        // convert output to array
+        Email[] actual = res.toArray(new Email[0]);
+        // convert input to array
+        Email[] inputArray = input.toArray(new Email[0]);
+        // expect the second element of input in output
+        Email[] expected = new Email[]{inputArray[1]};
         // check that output contains the last element
         Assertions.assertArrayEquals(expected, actual);
     }
