113d55d2d6addeace7cb266d2e485af016693b78
Fredins <fredin.martin@gmail.com>
2021-09-29 19:34:47 +0200

    replaced javax.activation with commons-email dep

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.group77.mejl {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.mail;
-    requires activation;
+    requires commons.email;
 
     exports org.group77.mejl;
     exports org.group77.mejl.controller;
