cc8507532659852dc8c916d3d6cd4006d18f91c5
elinhagman <73999747+elinhagman@users.noreply.github.com>
2021-10-06 10:43:21 +0200

    Added javadoc comment for initStoredAccounts + add labels to VBox instead of Flowpane

--- src/main/java/org/group77/mailMe/controller/StartPageController.java
+++ src/main/java/org/group77/mailMe/controller/StartPageController.java
@@ -1,5 +1,6 @@
 package org.group77.mailMe.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.geometry.*;
 import javafx.scene.Node;
@@ -13,34 +14,57 @@ import org.group77.mailMe.controller.utils.*;
 import org.group77.mailMe.model.*;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 
 
 public class StartPageController {
-  @FXML private Label welcomeLabel;
-  @FXML private BorderPane startPageBorderPane;
-  @FXML private Button addAccountButton;
-  @FXML private FlowPane accountsFlowPane;
+
+  @FXML
+  private BorderPane startPageBorderPane;
+
+  @FXML
+  private Label welcomeLabel;
+
+  @FXML
+  private Button addAccountButton;
+
+  @FXML
+  private FlowPane accountsFlowPane;
+
+  @FXML
+  private VBox accountsVbox;
 
   public void init(Model model) {
 
 
     // Add buttons for every stored accounts with actionEvent listeners
     initStoredAccounts(model);
-    initAddNewAccountButton(); // TODO add listener
-    /*
-    accountsListView.setOnMouseClicked(inputEvent -> {
-      ((Node) inputEvent.getSource()).getScene().getWindow().hide();
-    });
-    */
+    addAccountButton.setOnAction(actionEvent -> openAddAccount(
+                                      actionEvent,
+                                      model,
+                                      node -> ((Stage) node.getScene().getWindow()).close()));
+
   }
-  // TODO denna får du kommentera elin
+
+  /**
+   *
+   * Creates a label for each account in model's accounts and displays them in this VBox.
+   * Also adds listeners to each label that
+   *    1) sets model's active account to the one user pressed on
+   *    2) closes StartPage and opens Master
+   *
+   *
+   * @param model that holds the application state
+   */
 
   private void initStoredAccounts(Model model) {
     model.accounts.forEach(account -> {
+      // create labels for each email address in model
       Label accountLabel = new Label(account.emailAddress());
-      accountsFlowPane.getChildren().add(accountLabel);
+      // add to Vbox that is inside Flow Pane
+      accountsVbox.getChildren().add(accountLabel);
       // TODO: ev fit labels to dimensions
-
+      // add OnMouseClicked listeners to each email address label
       accountLabel.setOnMouseClicked(inputEvent -> {
         model.activeAccount.set(account);
         ((Stage)((Node) inputEvent.getSource()).getScene().getWindow()).close();
@@ -50,11 +74,12 @@ public class StartPageController {
     });
   }
 
-  private void initAddNewAccountButton() {
-
+  private void openAddAccount(ActionEvent actionEvent, Model model, Consumer<Node> onClose) {
+    ((Stage)((Node) actionEvent.getSource()).getScene().getWindow()).close();
+    WindowOpener.openMaster(model);
+    WindowOpener.openAddAccount(model,onClose);
+    // TODO: add account doesnt open a new StartPage.. so i do it here instead, maybe there is a better solution?
   }
 
-  public Button getAddAccountButton() {
-    return this.addAccountButton;
-  }
+
 }
