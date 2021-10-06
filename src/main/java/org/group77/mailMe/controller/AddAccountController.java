f11bc614862ab1e840fbb18796c39a626c4d05db
elinhagman <73999747+elinhagman@users.noreply.github.com>
2021-10-06 08:47:38 +0200

    AddAccount: Changed fxid's, added style sheet and added an error label

--- src/main/java/org/group77/mailMe/controller/AddAccountController.java
+++ src/main/java/org/group77/mailMe/controller/AddAccountController.java
@@ -4,22 +4,31 @@ import javafx.event.Event;
 import javafx.fxml.*;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.text.Text;
 import org.group77.mailMe.model.*;
 import org.group77.mailMe.model.data.*;
 import java.util.function.*;
 
 public class AddAccountController {
-  @FXML private TextField user;
-  @FXML private Button addAccountBtn;
-  @FXML private PasswordField passwordField;
+  @FXML
+  private TextField emailTextField;
+
+  @FXML
+  private PasswordField passwordField;
+
+  @FXML
+  private Button addAccountButton;
+
+  @FXML
+  private Label errorLabel;
 
   /**
    * add event handlers to nodes
    */
   public void init(Model model, Consumer<Node> onClose) {
     // input handlers
-    addAccountBtn.setOnAction(inputEvent -> addAccount(model, inputEvent, onClose));
-    passwordField.setOnAction(inputEvent -> addAccount(model, inputEvent, onClose));
+    addAccountButton.setOnAction(inputEvent -> addAccount(model, inputEvent, onClose));
+    //passwordField.setOnAction(inputEvent -> addAccount(model, inputEvent, onClose));
   }
 
   /**
@@ -28,12 +37,13 @@ public class AddAccountController {
    */
   private void addAccount(Model model, Event inputEvent, Consumer<Node> onClose) {
     try {
-      Account account = AccountFactory.createAccount(user.getText(), passwordField.getText().toCharArray());
+      Account account = AccountFactory.createAccount(emailTextField.getText(), passwordField.getText().toCharArray());
       model.addAccount(account);
       // call the closing function
       onClose.accept((Node) inputEvent.getSource());
     } catch (Exception e) {
-      e.printStackTrace(); // TODO feedback
+      // errorLabel.setText(e.getMessage()); TODO: give a good exception message in Model
+      errorLabel.setText("Wrong login credentials, or domain is not supported");
     }
   }
 
