3acfb8e0d3d7b9054dfafc18dbd07609024393dc
elinhagman <73999747+elinhagman@users.noreply.github.com>
2021-10-06 16:35:16 +0200

    Add test class for Folder and two tests in it

---
+++ src/test/java/org/group77/mailMe/model/TestFolder.java
@@ -0,0 +1,46 @@
+package org.group77.mailMe.model;
+
+
+import org.group77.mailMe.model.data.Email;
+import org.group77.mailMe.model.data.Folder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestFolder {
+
+    @Test
+    public void testFolderName() {
+        String a1 = "Inbox";
+
+        List<Email> emails = Arrays.asList(
+                new Email("test@gmail.com", new String[]{"77grupp@gmail.com"}, "Hello", "my message"),
+                new Email("test@gmail.com", new String[]{"77grupp@gmail.com"}, "Hello2", "another message"),
+                new Email("test@gmail.com", new String[]{"77grupp@gmail.com"}, "Hello3", "very important email"));
+
+        Folder folder = new Folder(a1, emails);
+
+        String a2 = folder.name();
+
+        Assertions.assertEquals(a1,a2);
+    }
+
+    @Test
+    public void testFolderEmails() {
+
+        List<Email> a1 = Arrays.asList(
+                new Email("test@gmail.com", new String[]{"77grupp@gmail.com"}, "Hello", "my message"),
+                new Email("test@gmail.com", new String[]{"77grupp@gmail.com"}, "Hello2", "another message"),
+                new Email("test@gmail.com", new String[]{"77grupp@gmail.com"}, "Hello3", "very important email"));
+
+        Folder folder = new Folder("Inbox", a1);
+        List<Email> a2 = folder.emails();
+
+        Assertions.assertEquals(a1,a2);
+    };
+
+
+}
