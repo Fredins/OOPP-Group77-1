ed9d801dd70897c25daa6e29e1bebfc5ce8358e0
elinhagman <73999747+elinhagman@users.noreply.github.com>
2021-10-11 19:07:35 +0200

    Made StartPageController fit with Control instead of model

--- src/main/java/org/group77/mailMe/controller/StartPageController.java
+++ src/main/java/org/group77/mailMe/controller/StartPageController.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import org.group77.mailMe.Main;
 import org.group77.mailMe.controller.utils.*;
 import org.group77.mailMe.model.*;
+import org.group77.mailMe.model.Control;
 import org.group77.mailMe.model.data.Account;
 
 import java.io.IOException;
@@ -32,11 +33,11 @@ public class StartPageController {
    * @param model holds the application state
    * @author Elin Hagman
    */
-  public void init(Model model) {
-    initStoredAccounts(model);
+  public void init(Control control) {
+    initStoredAccounts(control);
     addAccountButton.setOnAction(actionEvent -> openAddAccount(
                                       actionEvent,
-                                      model));
+                                      control));
   }
 
   /**
@@ -50,8 +51,8 @@ public class StartPageController {
    * @author Elin Hagman
    */
 
-  private void initStoredAccounts(Model model) {
-    for (Account account : model.accounts.get()) {
+  private void initStoredAccounts(Control control) {
+    for (Account account : control.getAccounts()) {
       try {
         // load AccountListItem.fxml
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("AccountListItem.fxml"));
@@ -62,8 +63,9 @@ public class StartPageController {
         // add OnMouseClicked to accountPane TODO: move to AccountListItemController??
         accountPane.setOnMouseClicked(inputEvent -> {
           ((Stage)((Node) inputEvent.getSource()).getScene().getWindow()).close();
-          WindowOpener.openMaster(model);
-          model.activeAccount.set(account);
+          WindowOpener.openMaster(control);
+          control.setActiveAccount(account);
+          // control.setActiveAccount(account)
         });
       } catch (IOException e) {
         e.printStackTrace();
@@ -79,14 +81,14 @@ public class StartPageController {
    * @param model holds the application state
    * @author Elin Hagman
    */
-  private void openAddAccount(ActionEvent actionEvent, Model model) {
+  private void openAddAccount(ActionEvent actionEvent, Control control) {
     ((Stage)((Node) actionEvent.getSource()).getScene().getWindow()).close();
     // onClose is a function for AddAccountView that determines how closing the view should be handled
     Consumer<Node> onClose = node -> {
       ((Stage) node.getScene().getWindow()).close();
-      WindowOpener.openMaster(model);
+      WindowOpener.openMaster(control);
     };
-    WindowOpener.openAddAccount(model,onClose);
+    WindowOpener.openAddAccount(control,onClose);
 
   }
 }
