4bf15b47646ce91e554077bf8e09b23ec617df7d
DavidZamanian <54624136+DavidZamanian@users.noreply.github.com>
2021-10-13 10:24:36 +0200

    Added choiceBox for time sorting and populated it

--- src/main/java/org/group77/mailMe/controller/FilterController.java
+++ src/main/java/org/group77/mailMe/controller/FilterController.java
@@ -2,6 +2,7 @@ package org.group77.mailMe.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import org.group77.mailMe.model.Model;
@@ -18,10 +19,11 @@ public class FilterController {
     @FXML private TextField toTextField;
     @FXML private TextField fromTextField;
     @FXML private DatePicker maxDatePicker;
+    @FXML private ChoiceBox<String> timeChoiceBox;
 
 
     /** init function
-     * 
+     *
      * @param model
      * @author David Zamanian
      */
@@ -35,5 +37,12 @@ public class FilterController {
         addFilterButton.setOnMouseClicked(inputEvent -> {
             System.out.println("Do something"); //TODO for Hampus to add
         });
+        populateChoiceBox();
+    }
+
+    private void populateChoiceBox(){
+        timeChoiceBox.getItems().clear();
+        timeChoiceBox.getItems().add(0, "Newest To Oldest");
+        timeChoiceBox.getItems().add(1, "Oldest To Newest");
     }
 }
