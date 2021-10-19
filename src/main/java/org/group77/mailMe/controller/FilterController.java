9c12c1a783b2a29ecab42cc53e3104b7060353ac
Hampus Jernkrook <hampus.e.jernkrook@gmail.com>
2021-10-19 12:08:05 +0200

    added additional comments

--- src/main/java/org/group77/mailMe/controller/FilterController.java
+++ src/main/java/org/group77/mailMe/controller/FilterController.java
@@ -19,29 +19,39 @@ import java.util.Objects;
  */
 
 public class FilterController {
-    @FXML private Button clearFilterButton; //button for clearing all filters
-    @FXML private Button applyFilterButton; //button for applying all chosen filters
-    @FXML private TextField toTextField; // text field for filtering on 'to'
-    @FXML private TextField fromTextField; // text field for filtering on 'from'
-    @FXML private DatePicker maxDatePicker; // date picker for filtering on max date.
-    @FXML private DatePicker minDatePicker; // date picker for filtering on min date.
-    @FXML private ChoiceBox<String> sortingChoiceBox; //list of sorting alternatives.
-    @FXML private AnchorPane rootPane; // the underlying pane which all other components are children of.
-    @FXML private Button closeButton; // button for closing the filter view.
+    @FXML
+    private Button clearFilterButton; //button for clearing all filters
+    @FXML
+    private Button applyFilterButton; //button for applying all chosen filters
+    @FXML
+    private TextField toTextField; // text field for filtering on 'to'
+    @FXML
+    private TextField fromTextField; // text field for filtering on 'from'
+    @FXML
+    private DatePicker maxDatePicker; // date picker for filtering on max date.
+    @FXML
+    private DatePicker minDatePicker; // date picker for filtering on min date.
+    @FXML
+    private ChoiceBox<String> sortingChoiceBox; //list of sorting alternatives.
+    @FXML
+    private AnchorPane rootPane; // the underlying pane which all other components are children of.
+    @FXML
+    private Button closeButton; // button for closing the filter view.
     // Strings for the sorting choice box:
     private final String newToOldSorting = "Newest to Oldest";
     private final String oldToNewSorting = "Oldest to Newest";
     // set new to old sorting as default
     private final String defaultSorting = newToOldSorting;
 
-    /** init function
+    /**
+     * Upon initialisation: add actions to all buttons and set default sorting.
      *
-     * @param control
+     * @param control - the class to delegate on to towards the backend.
      * @author David Zamanian
      * @author Hampus Jernkrook
      */
 
-    public void init(Control control){
+    public void init(Control control) {
         // upon clearing the filter, clear all fields except the sorting.
         // Sorting instead is set to default.
         clearFilterButton.setOnMouseClicked(inputEvent -> {
@@ -69,12 +79,23 @@ public class FilterController {
         // control.sortByNewToOld();
     }
 
-    private void populateChoiceBox(){
+    /**
+     * Populate the sorting choice box with sorting alternatives.
+     *
+     * @author David Zamanian
+     */
+    private void populateChoiceBox() {
         sortingChoiceBox.getItems().clear();
         sortingChoiceBox.getItems().add(0, newToOldSorting);
         sortingChoiceBox.getItems().add(1, oldToNewSorting);
     }
 
+    /**
+     * Applies all filters.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applyAllFilters(Control control) {
         // apply all filters chosen
         // if some string is entered in the to-field, then filter on to
@@ -85,24 +106,41 @@ public class FilterController {
         applySorting(control);
     }
 
+    /**
+     * Applies the filter on 'to'.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applyToFilter(Control control) {
         // if some string is entered in the to-field, then filter on to
         if (!Objects.equals(toTextField.getText(), "")) {
-            System.out.println("GOING TO FILTER ON TO"); //TODO REMOVE
             control.filterOnTo(toTextField.getText());
         }
     }
 
+    /**
+     * Applies the filter on 'from'.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applyFromFilter(Control control) {
+        // if some text is entered into the text field, then apply the filtering.
         if (!Objects.equals(fromTextField.getText(), "")) {
-            System.out.println("GOING TO FILTER ON FROM"); //TODO REMOVE
             control.filterOnFrom(fromTextField.getText());
         }
     }
 
+    /**
+     * Applies the filter on max date.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applyMaxDateFilter(Control control) {
+        // if some max date is chosen, filter on that max date.
         if (!Objects.equals(maxDatePicker.getValue(), null)) {
-            System.out.println("GOING TO FILTER ON MAX DATE"); //todo remove
             // convert the selected date to LocalDateTime.
             // For max date to be inclusive, set max date to be start of day after the chosen date.
             LocalDateTime date = maxDatePicker.getValue().plusDays(1).atStartOfDay();
@@ -110,28 +148,43 @@ public class FilterController {
         }
     }
 
+    /**
+     * Applies the filter on min date.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applyMinDateFilter(Control control) {
+        // if some min date is chosen, filter on that min date.
         if (!Objects.equals(minDatePicker.getValue(), null)) {
-            System.out.println("GOING TO FILTER ON MIN DATE"); //todo remove
             // convert the selected date to LocalDateTime. Min date is inclusive.
             LocalDateTime date = minDatePicker.getValue().atStartOfDay();
             control.filterOnMinDate(date);
         }
     }
 
+    /**
+     * Applies the chosen sorting order.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applySorting(Control control) {
+        // if there is some sorting choice made, then apply the chosen order.
         if (!Objects.equals(sortingChoiceBox.getValue(), null)) {
-            System.out.println("TIME CHOICE BOX IS NOT EMPTY"); //TODO REMOVE
             if (Objects.equals(sortingChoiceBox.getValue(), newToOldSorting)) {
-                System.out.println("NEW TO OLD SORTING APPLIED"); //TODO REMOVE
                 control.sortByNewToOld();
             } else if (Objects.equals(sortingChoiceBox.getValue(), oldToNewSorting)) {
-                System.out.println("OLD TO NEW SORTING APPLIED"); //TODO REMOVE
                 control.sortByOldToNew();
             }
         }
     }
 
+    /**
+     * Clear all filters and sets the sorting choice to default.
+     *
+     * @author Hampus Jernkrook
+     */
     private void clearAllFilters() {
         toTextField.clear();
         fromTextField.clear();
@@ -140,6 +193,12 @@ public class FilterController {
         sortingChoiceBox.setValue(defaultSorting);
     }
 
+    /**
+     * Applies the default sorting: new to old emails.
+     *
+     * @param control - the class to delegate on to.
+     * @author Hampus Jernkrook
+     */
     private void applyDefaultSorting(Control control) {
         control.sortByNewToOld();
     }
