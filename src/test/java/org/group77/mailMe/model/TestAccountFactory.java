12bcd2ac9d3b66f16c83c3cef4a79289333947fa
Fredins <fredin.martin@gmail.com>
2021-10-24 16:24:51 +0200

    added test to AccountFactory for full coverage, and improved a test as well

--- src/test/java/org/group77/mailMe/model/TestAccountFactory.java
+++ src/test/java/org/group77/mailMe/model/TestAccountFactory.java
@@ -4,20 +4,31 @@ import org.junit.jupiter.api.*;
 public class TestAccountFactory {
 
     /**
-     * @author Martin
+     * @author Martin Fredin
      */
     @Test
-    void testSupportedDomain(){
-        Assertions.assertDoesNotThrow(() ->
-          AccountFactory.createAccount("name@gmail.com", "pwd".toCharArray()));
+    void testGmail() throws EmailDomainNotSupportedException {
+        Account account = AccountFactory.createAccount("name@gmail.com", "pwd".toCharArray());
+        Assertions.assertEquals(account.provider(), ServerProvider.GMAIL);
     }
 
     /**
-     * @author Martin
+     * @author Martin Fredin
+     */
+    @Test
+    void testMicrosoft() throws EmailDomainNotSupportedException {
+        Account account = AccountFactory.createAccount("name@outlook.com", "pwd".toCharArray());
+        Assertions.assertEquals(account.provider(), ServerProvider.MICROSOFT);
+    }
+
+    /**
+     * @author Martin Fredin
      */
     @Test
     void testNotSupportedDomain(){
         Assertions.assertThrows(EmailDomainNotSupportedException.class, () ->
           AccountFactory.createAccount("gmail@wrongDomain.com", "pwd".toCharArray()));
     }
+
+
 }
